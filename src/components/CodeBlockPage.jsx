2c5791314c96657352ceb4cd64b482f1d90d560b
ShovalElhaiany <shoval.elhaiany@gmail.com>
2024-02-04 22:07:48 +0200

    https://online-coding-app-server-production.up.railway.app

--- src/components/CodeBlockPage.jsx
+++ src/components/CodeBlockPage.jsx
@@ -11,7 +11,7 @@ const CodeBlockPage = ({ codeBlock }) => {
 
   // Effect hook to set up socket connection and handle code updates
   useEffect(() => {
-    const newSocket = io("online-coding-app-server.railway.internal:5000");
+    const newSocket = io("https://online-coding-app-server-production.up.railway.app");
     newSocket.on("connect", () => {
       setSocket(newSocket);
     });
@@ -31,7 +31,7 @@ const CodeBlockPage = ({ codeBlock }) => {
 
   useEffect(() => {
     const checkIfMentor = async () => {
-      const response = await fetch("http://online-coding-app-server.railway.internal:5000/user_info", {
+      const response = await fetch("https://online-coding-app-server-production.up.railway.app/user_info", {
         headers: {
           "Content-Type": "application/json",
         },
@@ -43,7 +43,7 @@ const CodeBlockPage = ({ codeBlock }) => {
       // If user is a mentor, send a post request
       if (data.isMentor) {
         alert("Hi Tom :)");
-        await fetch("http://online-coding-app-server.railway.internal:5000/user_info", {
+        await fetch("https://online-coding-app-server-production.up.railway.app/user_info", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
